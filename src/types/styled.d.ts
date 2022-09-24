4a9432f68e7136c9e97c405e8a3036a459a1fab3
David Rappa <davidrappa@outlook.com>
2022-09-24 12:46:38 -0300

    styles(Theme): added colors the theme inside project and types them

---
+++ src/types/styled.d.ts
@@ -0,0 +1,74 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      "type-bug": string;
+      "type-dragon": string;
+      "type-dark": string;
+      "type-electric": string;
+      "type-fairy": string;
+      "type-fighting": string;
+      "type-fire": string;
+      "type-flying": string;
+      "type-ghost": string;
+      "type-grass": string;
+      "type-ground": string;
+      "type-ice": string;
+      "type-normal": string;
+      "type-poison": string;
+      "type-psychic": string;
+      "type-rock": string;
+      "type-steel": string;
+      "type-water": string;
+
+      "background-type-bug": string;
+      "background-type-dragon": string;
+      "background-type-dark": string;
+      "background-type-electric": string;
+      "background-type-fairy": string;
+      "background-type-fighting": string;
+      "background-type-fire": string;
+      "background-type-flying": string;
+      "background-type-ghost": string;
+      "background-type-grass": string;
+      "background-type-ground": string;
+      "background-type-ice": string;
+      "background-type-normal": string;
+      "background-type-poison": string;
+      "background-type-psychic": string;
+      "background-type-rock": string;
+      "background-type-steel": string;
+      "background-type-water": string;
+
+      "background-white": string;
+      "background-default-input": string;
+      "background-pressed-input": string;
+      "background-modal": string;
+
+      "text-white": string;
+      "text-black": string;
+      "text-gray": string;
+      "text-number": string;
+
+      "height-short": string;
+      "height-medium": string;
+      "height-tall": string;
+
+      "weight-light": string;
+      "weight-medium": string;
+      "weight-heavy": string;
+    };
+    fontSizes: {
+      "font-size-32": number;
+      "font-size-26": number;
+      "font-size-16": number;
+      "font-size-12": number;
+    };
+    fontWeights: {
+      "font-weight-light": string;
+      "font-weight-regular": string;
+      "font-weight-bold": string;
+    };
+  }
+}
